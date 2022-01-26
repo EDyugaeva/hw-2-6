e9c666bcf54a4746a38fdeb36b45b39a69a21be3
EDyugaeva <ed2408@yandex.ru>
2022-01-26 22:37:15 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/hw26/Homework.java
@@ -0,0 +1,12 @@
+package pro.sky.java.course2.hw26;
+
+import org.springframework.boot.SpringApplication;
+
+public class Homework {
+    public static void main(String[] args) {
+
+        
+
+}
+
+}
